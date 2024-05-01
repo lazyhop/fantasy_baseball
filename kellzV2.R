320bb6beae46951e5255cce1a9bc8b3aaea4dac1
ajonih <ajonihopkins2018@u.northwestern.edu>
2024-05-01 14:25:17 -0500

    creating a markdown file to develop a report, and thoughts on filteroptions for Shiny development. Created static datafiles of the pulls from Yahoo's API.

--- kellzV2.R
+++ kellzV2.R
@@ -13,14 +13,20 @@ my_token <- y_create_token(my_key = 'dj0yJmk9R2VtbXNoaVNhVmFYJmQ9WVdrOWFYQmxaVE5
 my_token$can_refresh() # check if token is refreshable
 my_token$refresh() # refresh token
 
-# Pulling stats from Yahoo ----
-teams <- y_teams(league_key = '422.l.78145', token_name = my_token)
-allWeeks <- y_team_stats(key = teams$team_key, my_token, week = c(1:13))
+# league information ----
+games <- list()
+games <- y_games(my_token)
 
-stat_cats <- y_stats_categories(game_key = '422', my_token) %>%
+
+# Pulling stats from Yahoo ----
+teams <- y_teams(league_key = '431.l.93970', token_name = my_token)
+allWeeks <- y_team_stats(key = teams$team_key, my_token, week = c(1:24))
+
+stat_cats <- y_stats_categories(game_key = '431', my_token) %>%
   select(stat_id, display_name, position_type)
  
 allWeekStats <- allWeeks %>%
+  filter(team_remaining_games == 0) %>% # only getting info for completed weeks
   select(team_stats_week, team_key, team_name, team_stats) %>%
   unnest(team_stats)
 
@@ -170,56 +176,44 @@ zScoreSumsWeek <- function(weekNum){
 }
 
 
-actualValues <- actualValuesWeek('13')
-rankings <- rankingsWeek('11')
-zScore <- zScoreWeek('12')
-zScoreSums <- zScoreSumsWeek('12')
+actualValues <- actualValuesWeek('3')
+rankings <- rankingsWeek('3')
+zScore <- zScoreWeek('3')
+zScoreSums <- zScoreSumsWeek('3')
 
 # Creating ZScoreSums Table ----
 zScoreSums <- tibble()
 
-for (c in 1:13){
+for (c in 1:4){
   zScoreSumsadd <- zScoreSumsWeek(c)
   
   zScoreSums <- rbind(zScoreSums, zScoreSumsadd)
 }
 
 zScoreSums2 <- zScoreSums %>%
-  gather(key = 'team_name', value = 'zScoreSum', 4:13)
+  gather(key = 'team_name', value = 'zScoreSum', 4:15)
 
 # Visualization ----
 statsList <- c('R', 'HR', 'RBI', 'SB', 'OPS',
                'W', 'K', 'ERA', 'WHIP', 'SVH')
 
-# Compare stats, 1 Team, hitting
+# Compare stats, 1 Team, hitting x
 allWeekStats3 %>%
-  filter(position_type == 'B', grepl('Cuca',team_name)) %>%
+  filter(position_type == 'B', grepl('Awesome',team_name)) %>%
   ggplot(aes(x = as.numeric(team_stats_week), y = zScore, color = display_name)) +
   geom_line() +
   geom_point()
 
 # Mean z-score by category ----
-# hitting
 allWeekStats3 %>%
-  filter(position_type == 'B', grepl('Awesome',team_name),
-         as.numeric(team_stats_week) > 8) %>%
+  filter(position_type == 'B', grepl('Awesome',team_name)) %>%
+  #filter(as.numeric(team_stats_week) > 8) %>% # week filter for closer views
   group_by(display_name) %>%
-  summarise(zScore = mean(zScore)) %>%
+  summarise(zScore = mean(zScore, na.rm = TRUE)) %>%
   ggplot(aes(x = display_name, y = zScore)) +
     geom_col()
 # target ops!
 # preferably at 3b
-
-# pitching
-allWeekStats3 %>%
-  filter(position_type == 'P', grepl('Awesome',team_name)) %>%
-  group_by(display_name) %>%
-  summarise(zScore = mean(zScore)) %>%
-  ggplot(aes(x = display_name, y = zScore)) +
-  geom_col()
-# target whip, trade away K!
-# i need to flip teh values for era and whip!!!!!!!!
-# nola or burnes?
 
 # Compare stats, 1 Team, pitching
 allWeekStats3 %>%
@@ -227,6 +221,18 @@ allWeekStats3 %>%
   ggplot(aes(x = as.numeric(team_stats_week), y = zScore, color = display_name)) +
   geom_line() +
   geom_point()
+
+
+# pitching
+allWeekStats3 %>%
+  filter(position_type == 'P', grepl('Awesome',team_name)) %>%
+  group_by(display_name) %>%
+  summarise(zScore = mean(zScore, na.rm = TRUE)) %>%
+  ggplot(aes(x = display_name, y = zScore)) +
+  geom_col()
+# target whip, trade away K!
+# i need to flip teh values for era and whip!!!!!!!!
+# nola or burnes?
 
 
 # Compare Teams, 1 Stat
@@ -237,28 +243,28 @@ allWeekStats3 %>%
     geom_line() +
     geom_point()
 
+# Compare one stat for all teams
 allWeekStats3 %>%
   filter(display_name == 'OPS') %>%
   #filter(display_name == 'OPS', as.numeric(team_stats_week) > 8) %>%
   group_by(team_name) %>%
-  summarise(zScore = mean(zScore)) %>%
+  summarise(zScore = mean(zScore, na.rm = TRUE)) %>%
   ggplot(aes(x = team_name, y = zScore)) +
-    geom_col()
-
-
+    geom_col() +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))
 
 # zScore, Compare stats 1 team
 zScoreSums2 %>%
   filter(grepl('Awesome',team_name)) %>%
-  ggplot(aes(x = weekNumber, y = zScoreSum, color = position_type)) +
+  ggplot(aes(x = weekNumber, y = zScoreSum, color = position_type, group = position_type)) +
   geom_line() +
   geom_point()
 
-zScoreSums2 %>%
-  filter(grepl('Aweso',team_name)) %>%
-  ggplot(aes(x = weekNumber, y = zScoreSum, color = position_type)) +
-  geom_line() +
-  geom_point()
+# zScoreSums2 %>%
+#   filter(grepl('Aweso',team_name)) %>%
+#   ggplot(aes(x = weekNumber, y = zScoreSum, color = position_type)) +
+#   geom_line() +
+#   geom_point()
 
 # Total ZScore by Team
 zScoreSums2 %>%
@@ -284,6 +290,15 @@ zScoreSums2 %>%
 # create packet for the last 2 weeks!
 # create packet for the last 4 weeks
 # create packet for the last 6 weeks
+
+
+# shiny site
+# dashboard to view all team info
+# dashboard to filter visuals for one team!
+  # stats over times, batting, pitching,
+  # mean zscore for each stat for the selected team!
+  # would you win visual?
+
   
 
 
